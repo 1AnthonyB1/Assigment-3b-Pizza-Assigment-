46cfc13a4bc1a247b99e6dbbe0f9a8aa8070a8cb
1AnthonyB1 <124379293+1AnthonyB1@users.noreply.github.com>
2023-04-11 14:21:32 +0000

    Added Size and Toppings Calculations

--- Assigment_3b.py
+++ Assigment_3b.py
@@ -1,1 +1,18 @@
+#toppings = int(input(""))
 
+Size = (input("\nWhat Size of Pizza would you like: \nLarge? or Extra Large? \nType Size:"))
+if Size == "Large":
+    Price = 6.00
+elif Size == "Extra Large":
+     Price = 10.00
+
+Toppings = (input("\nWhat Number of toppings do you want: \n 1,2, 3 or 4? \nType Number of Toppings:"))
+if Toppings == "1":
+    Price = Price + 1.00
+elif Toppings == "2":
+     Price = Price + 1.75 
+elif Toppings == "3":
+     Price = Price + 2.50 
+elif Toppings == "4":
+     Price = Price + 3.35 
+print(Price)
